7ee9358dd9e7b703b2b5c9f0148d9ca34c5a2455
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-02-20 11:05:06 +0530

    [VISHNUVDEV] added Functional Programs

---
+++ Functional/HarmonicNumber/Harmonic.js
@@ -0,0 +1,17 @@
+/***************************************************************
+* @Execution : default node : cmd> harmonic.js
+* @Purpose : to study the functionals working.
+* @description : Harmonic numbers
+* @overview : Sum of harmonic number series
+* @author : Vishnu V Dev <vishnuvdev17@gmail.com>
+* @version : 1.0
+* @since : February 18 2020
+****************************************************************/
+
+const Harmonic=require('./harmonicBL')
+let input=require('readline-sync')
+let N=input.questionInt('enter the value of N')
+console.log(nthHarmonic(N))
+return 0
+
+Harmonic.nthHarmonic(N)
