32cc0f70142d80f0a83160a52799780251e41c9c
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-02-20 12:09:12 +0530

    [VISHNUVDEV] Functional Programs

---
+++ Functional/Flipcoin/Flipcoin.js
@@ -0,0 +1,16 @@
+/***************************************************************
+* @Execution : default node : cmd> Flipcoin.js
+* @Purpose : to study the functionals working.
+* @description : Flipcoin
+* @overview : Number of heads and tails while flipping a coin for n times
+* @author : Vishnu V Dev <vishnuvdev17@gmail.com>
+* @version : 1.0
+* @since : February 18 2020
+****************************************************************/
+
+const Flipcoin = require('./FlipcoinBL')
+var read =  require('readline-sync')
+let num =   read.question('enter the number of times you want to flip the coin ');
+console.log(Flip(num))
+return 0
+Flipcoin.Flip(num)
---
+++ Functional/Flipcoin/FlipcoinBL.js
@@ -0,0 +1,36 @@
+/**
+* @description : Flipcoin
+* @param {Coin Flip}
+* @return displays the number of heads and tails and percentage of the same while flipping a coin
+*/
+
+Flip=(num) =>
+{
+try {
+let heads = 0;
+let tails = 0;
+for(i=0;i<num;i++)
+{
+x = Math.floor(Math.random() * 10);
+console.log(x)
+if(x>5)
+{
+    tails++
+}
+else
+{
+    heads++
+}
+}
+console.log('number of heads is ',heads)
+console.log("number of tails is ",tails)
+headp = heads/num *100 ;
+tailp = tails/num *100;
+console.log('heads percentage =',headp);
+console.log('tails percentage = ',tailp);   
+} catch (error) {
+    console.log(error)
+    
+}
+}
+module.exports= {Flip}
