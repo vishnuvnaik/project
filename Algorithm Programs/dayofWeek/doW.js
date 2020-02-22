4b99649a6a4c5a7ad9c26e8b30fc1f837159e51f
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-02-22 15:16:59 +0530

    [Vishnu V Dev] added

---
+++ Algorithm Programs/dayofWeek/doW.js
@@ -0,0 +1,17 @@
+/***************************************************************
+* @Execution : default node : cmd> doW.js
+* @Purpose : to study the working of algorithm based programs
+* @description : Inputting the dates and output the respected day of week
+* @overview : Display the day of week as output
+* @author : Vishnu V Dev <vishnuvdev17@gmail.com>
+* @version : 1.0
+* @since : February 18 2020
+****************************************************************/
+const doW = require('./dowBL')
+let input = require('readline-sync')
+let m = input.questionInt('enter the month ');
+let d = input.questionInt('enter the date');
+let y = input.questionInt('enter the year ');
+console.log(dayofWeek(m,d,y))
+return 0;
+doW.dayofWeek(m,d,y);
