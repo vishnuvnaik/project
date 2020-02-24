1b21d31e35b89b57e78621b9a4614a7133821af1
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-02-24 15:24:19 +0530

    [Vishnu V Dev] ADD refactor Coupon

--- Functional/Coupon/CouponBL.js
+++ Functional/Coupon/CouponBL.js
@@ -4,20 +4,30 @@
 * @return Displays random coupon numbers accrording to the user desired times
 */
 
-couponCode = (n) => {
-    try {
-        for (i = 0; i < n; i++) // for loop to generate random numbers till n times
-        {
-            x = Math.floor(Math.random() * 10); // generating random numbers to form coupon codes
-            y = Math.floor(Math.random() * 100);
-            z = Math.floor(Math.random() * 1000);
-            a = Math.floor(Math.random() * 25);
-            console.log("the coupon numbers are", x, y, z, a); // printing coupon codes
+couponCode = (number) => {
+ 
+        try {
+            let n = 0;
+            let arr = [];
+            let rannum;
+            while (n < number) {
+                /**
+                 * Loop over till where num is equal to userinput to generate the coupon number.
+                 */
+                rannum = Math.round(Math.random() * 123456789);
+                if (!arr.includes(rannum)) {
+                    arr.push(rannum)
+                }
+                /**
+                 * Condition pushs the ranrandom number to the array if the are unique.
+                 * 
+                 */
+                n++;
+            }
+            console.log("Coupon Numbers :" + arr);
+
+        } catch (error) {
+            console.log(error.message);
+
         }
     }
-    catch (error) {
-        console.log(error)
-
-    }
-}
-module.exports = { couponCode }
