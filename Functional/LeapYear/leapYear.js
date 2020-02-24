e2f11f4fa03c705755db4e678aef21dabccc974c
Vishnu V Dev <vishnuvdev17@gmail.com>
2020-02-24 15:55:32 +0530

    [Vishnu V Dev] refactor.modified leapYear

--- Functional/LeapYear/leapYear.js
+++ Functional/LeapYear/leapYear.js
@@ -1,8 +1,8 @@
 /**************************************************************
-* @Execution : default node : cmd> triplets.js
+* @Execution : default node : cmd> leapYear.js
 * @Purpose : to study the functionals working.
-* @description : A program with cubic running time. Read in N integers and counts the number of triples that sum to exactly 0.
-* @overview : Sum of 3 entered integers is zero 
+* @description : Input the year and check wheather the enetered year is a leap year or not.
+* @overview : Leap year or not a leap year.
 * @author : Vishnu V Dev <vishnuvdev17@gmail.com>
 * @version : 1.0
 * @since : February 18 2020
--- Functional/LeapYear/leapYearBl.js
+++ Functional/LeapYear/leapYearBl.js
@@ -1,15 +1,15 @@
 /**
-* @description : A program with cubic running time. Read in N integers and counts the number of triples that sum to exactly 0.
+* @description : Input the year and check wheather the entered year is leap year or not
 * @purpose : Prints all triplets in array with 0 sum
-* @return displays the solution of the quadratic equation
+* @return displays wheather the entered year is a leap year or not
 */
 
-leapYearmain = (year) => {
+leapYearmain = (year) => { //function to check the input and display the output
     try {
-        if (year.length < 4) {
+        if (year.length < 4) { //check wheather the year has 4 digits or not
             console.log(" enter valid year");
         }
-        else {
+        else { //check for the leap year condition and display the result
             if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
                 console.log(year ," is Leap year")
             }
