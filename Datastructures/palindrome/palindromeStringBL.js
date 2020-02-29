21e5be3d9ecb99ef6a996cb33419615397ba3e10
vishnuvnaik <vishnuvdev17@gmail.com>
2020-02-29 18:25:17 +0530

    [Vishnu V Dev].Refactor palindromeStringBl.js

--- Datastructures/palindrome/palindromeStringBL.js
+++ Datastructures/palindrome/palindromeStringBL.js
@@ -7,53 +7,73 @@ exports.palindromString = (str) => {
     try {
         class Deque { // creating queue class
             constructor() {
-                this.Dequeue = [];
+                this.Deque = [];
+                this.items = {};
+                this.count = 0;
+                this.lowestcount = 0;
             }
-            addFront(data) { // adds an element to the queue
-                this.Queue.push(data); //adding element to the queue
+            addRear(data) { // adds an element to the queue
+                this.Deque.push(data);
+                this.count++; //adding element to the queue
+            }
+            addFront(data) {
+                if (this.empty) {
+                    this.addRear(data);
+                } else if (this.lowestcount > 0) {
+                    this.lowestcount--;
+                } else {
+                    for (let index = this.count; index > 0; index--) {
+                        this.items[index] = this.items[index - 1];
+                    }
+                    this.count++;
+                    this.items[0] = data;
+                }
+                return true;
             }
             removeFront() { //removes an element
-                return this.Queue.shift(); //removing and element from the queue
+                if (this.isEmpty) {
+                    return undefined;
+                }
+                let result = this.items[this.lowestCount];
+                delete this.items[this.lowestCount];
+                this.lowestCount++;
+                return result;
             }
             isEmpty() { //checks wheather the queue is empty or not
-                if (this.Queue.length === 0) {
-                    return true;
-                } else {
-                    false;
-                }
+                return this.length == 0;
 
             }
             length() {
-                return this.Queue.length;
+                return this.count - this.lowestCount;
             }
             front() { // returns the front element of the queue without removing it
                 if (this.empty) {
                     console.print(" no data");
                 } else {
-                    return this.Queue;
+                    return this.Deque;
                 }
             }
             printQue() { //function to print the queue
                 let string = '';
                 let i = 0;
-                while (i < this.Queue.length) {
-                    string = string + this.Queue[i] + '';
+                while (i < this.Deque.length) {
+                    string = string + this.Deque[i] + '';
                     i++;
                 }
                 console.log(string);
             }
         }
-        var quenew = new Queue;
+        var quenew = new Deque;
         let i = 0;
         if (str.length >= 2) {
             while (i < str.length) {
-                quenew.enqueue(str[i]);
+                quenew.addFront(str[i]);
                 i++
             }
             let j = quenew.length();
             while (j >= 0) {
                 if (quenew.length() == str[j]) {
-                    quenew.dequeue();
+                    quenew.removeFront();
                 }
                 j--;
 
