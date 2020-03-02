94aa14924826ea8325d73735fdb462e23f8e0aa7
vishnuvnaik <vishnuvdev17@gmail.com>
2020-03-02 17:39:06 +0530

    [Vishnu V Dev].Added primeAnagram

---
+++ Datastructures/primeAnagram/prime.js
@@ -0,0 +1,3 @@
+const path = require('./primeBL')
+let arr = new Array(10);
+path.findPrime(arr);
---
+++ Datastructures/primeAnagram/primeBL.js
@@ -0,0 +1,46 @@
+exports.findPrime = (arr) => {
+    try {
+        function isPrime(num) {
+            for (let i = 2; i <= num / 2; i++) {
+                if (num % i === 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = new Array(50);
+        }
+        let j = 0,
+            k = 0;
+        for (let i = 2; i <= 1000; i++) {
+            if (i % 100 === 0) {
+                k = 0;
+                j++
+            }
+            let res = isPrime(i);
+            if (res) {
+                arr[j][k] = i;
+                k++
+            }
+        }
+        console.log('Prime no:')
+        console.log(arr)
+        let arra = new Array(2);
+        for (let i = 0; i < arra.length; i++) {
+            arra[i] = new Array();
+        }
+        let ar = [];
+        for (let i = 2; i <= 1000; i++) {
+            let res = isPrime(i);
+
+            if (res) {
+                ar.push(i);
+            }
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
